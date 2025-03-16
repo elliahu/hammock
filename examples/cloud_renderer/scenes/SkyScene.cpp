32b549cc45123383a65a96d54f86cb070a28f57a
Matěj Eliáš <matej.elias@vsb.cz>
2025-03-16 19:15:22 +0100

    adjusted values

--- examples/cloud_renderer/scenes/SkyScene.cpp
+++ examples/cloud_renderer/scenes/SkyScene.cpp
@@ -640,7 +640,7 @@ void SkyScene::buildRenderGraph() {
                     ImGui::PlotLines("Frame Times", frameTimes, FRAMETIME_BUFFER_SIZE, frameTimeFrameIndex, nullptr, 0.0f, 33.0f,
                                      ImVec2(0, 80));
                     ImGui::SeparatorText("Rendering");
-                    ImGui::DragInt("Maximum number of samples", &computePushConsts.DEBUG_maxSamples);
+                    ImGui::DragInt("Max samples", &computePushConsts.DEBUG_maxSamples, 1, 0, 2048);
 
                     ImGui::SeparatorText("Debug views");
                     ImGui::Checkbox("Expensive light sampling", (bool*)&computePushConsts.DEBUG_expensiveSampling);
