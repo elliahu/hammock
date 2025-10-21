a2cd976c3a09fa210f683034a8908447cd854e10
Matej Elias <elias.matysek@gmail.com>
2025-10-21 13:36:01 +0200

    pass builder added

--- examples/render_graph/main.cpp
+++ examples/render_graph/main.cpp
@@ -2,35 +2,36 @@
 
 using namespace Hammock;
 
-std::string assetPath(const std::string &asset) {
+std::string assetPath(const std::string& asset) {
     return "../../../data/" + asset;
 }
 
-std::string compiledShaderPath(const std::string &shader) {
+std::string compiledShaderPath(const std::string& shader) {
     return "../../../src/hammock/shaders/compiled/" + shader + ".spv";
 }
 
 // Here we create a simple logical render pass
 class SimplePass final : public Rendergraph::ILogicalRenderPass {
-public:
-    explicit SimplePass(const CreateInfo &createInfo)
+   public:
+    explicit SimplePass(const CreateInfo& createInfo)
         : ILogicalRenderPass(createInfo) {
     }
 
-    void create() override {
+    // Called by render graph, Allocates resources
+    void onCreate() override {
         Logger::info("Creating SimplePass");
     }
 
-    void release() override {
-
+    // Called by render graph Frees allocated resources
+    void onRelease() override {
     }
 
-    void declareResources() override {
-
+    // Called by render graph once to build the graph, Declare logical resources that are used
+    void onDeclareResources() override {
     }
 
-    void recordCommands(VkCommandBuffer) override {
-
+    // Called by render graph for every frame
+    void onRecordCommands(VkCommandBuffer) override {
     }
 };
 
@@ -39,34 +40,44 @@ int main() {
     Hammock::Window window{instance, "Render Graph", 1920, 1080};
     Device device{instance, window.getSurface()};
     ResourceManager rm{device};
-    // TODO decouple context and window
     FrameManager fm{window, device, rm};
     std::unique_ptr<DescriptorPool> descriptorPool = DescriptorPool::Builder(device)
-            .setMaxSets(20000)
-            .setPoolFlags(VK_DESCRIPTOR_POOL_CREATE_FREE_DESCRIPTOR_SET_BIT)
-            .addPoolSize(VK_DESCRIPTOR_TYPE_SAMPLER, 10000)
-            .addPoolSize(VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, 10000)
-            .addPoolSize(VK_DESCRIPTOR_TYPE_SAMPLED_IMAGE, 10000)
-            .addPoolSize(VK_DESCRIPTOR_TYPE_STORAGE_IMAGE, 10000)
-            .addPoolSize(VK_DESCRIPTOR_TYPE_UNIFORM_TEXEL_BUFFER, 10000)
-            .addPoolSize(VK_DESCRIPTOR_TYPE_STORAGE_TEXEL_BUFFER, 10000)
-            .addPoolSize(VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, 10000)
-            .addPoolSize(VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, 10000)
-            .addPoolSize(VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER_DYNAMIC, 10000)
-            .addPoolSize(VK_DESCRIPTOR_TYPE_STORAGE_BUFFER_DYNAMIC, 10000)
-            .addPoolSize(VK_DESCRIPTOR_TYPE_INPUT_ATTACHMENT, 10000)
-            .build();
+                                                         .setMaxSets(20000)
+                                                         .setPoolFlags(VK_DESCRIPTOR_POOL_CREATE_FREE_DESCRIPTOR_SET_BIT)
+                                                         .addPoolSize(VK_DESCRIPTOR_TYPE_SAMPLER, 10000)
+                                                         .addPoolSize(VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, 10000)
+                                                         .addPoolSize(VK_DESCRIPTOR_TYPE_SAMPLED_IMAGE, 10000)
+                                                         .addPoolSize(VK_DESCRIPTOR_TYPE_STORAGE_IMAGE, 10000)
+                                                         .addPoolSize(VK_DESCRIPTOR_TYPE_UNIFORM_TEXEL_BUFFER, 10000)
+                                                         .addPoolSize(VK_DESCRIPTOR_TYPE_STORAGE_TEXEL_BUFFER, 10000)
+                                                         .addPoolSize(VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, 10000)
+                                                         .addPoolSize(VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, 10000)
+                                                         .addPoolSize(VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER_DYNAMIC, 10000)
+                                                         .addPoolSize(VK_DESCRIPTOR_TYPE_STORAGE_BUFFER_DYNAMIC, 10000)
+                                                         .addPoolSize(VK_DESCRIPTOR_TYPE_INPUT_ATTACHMENT, 10000)
+                                                         .build();
 
     // Create the rendergraph instance
     auto renderGraph = std::make_unique<Rendergraph::Graph>(Rendergraph::Graph::CreateInfo{
-        .resourceManager = rm
-    });
+        .resourceManager = rm});
 
-    renderGraph->addPass(std::make_unique<SimplePass>(Rendergraph::ILogicalRenderPass::CreateInfo{
+    // Add the pass
+    /*renderGraph->addPass(std::make_unique<SimplePass>(Rendergraph::ILogicalRenderPass::CreateInfo{
         .commandQueueFamily = CommandQueueFamily::Graphics,
         .resourceManager = rm,
-    }));
+    }));*/
 
+    renderGraph->addPass(
+        Rendergraph::LogicalPassBuilder::create(CommandQueueFamily::Graphics, rm)
+            .onCreate([] { Logger::info("Small pass created"); })
+            .onRelease([] { Logger::info("Small pass released"); })
+            .onDeclareResources([](Rendergraph::ILogicalRenderPass& pass) {
+                // pass.read(...); pass.write(...);
+            })
+            .onRecordCommands([](VkCommandBuffer cmd) {
+                // vkCmdBindPipeline(...);
+            })
+            .build());
 
     device.waitIdle();
     while (!window.shouldClose()) {
--- include/hammock/rendergraph/ILogicalRenderPass.h
+++ include/hammock/rendergraph/ILogicalRenderPass.h
@@ -8,48 +8,132 @@ namespace Hammock {
         /// RenderPass interface
         /// Inherit from this interface to create a concrete render pass
         class ILogicalRenderPass {
-        public:
+           public:
             // This is called when pass is created, initialize all needed resources only in here
-            virtual void create() = 0;
+            virtual void onCreate() = 0;
 
             // Release all allocated resources here
-            virtual void release() = 0;
+            virtual void onRelease() = 0;
 
             struct CreateInfo {
-                const CommandQueueFamily &commandQueueFamily;
-                ResourceManager &resourceManager;
+                const CommandQueueFamily& commandQueueFamily;
+                ResourceManager& resourceManager;
             };
 
-            explicit ILogicalRenderPass(const CreateInfo &createInfo)
+            explicit ILogicalRenderPass(const CreateInfo& createInfo)
                 : resourceManager(createInfo.resourceManager), commandQueueFamily(createInfo.commandQueueFamily) {
             }
 
             // Declare what resources this pass will read/write
             // Call read(...) to declare read access
             // Call write(...) to declare write access
-            virtual void declareResources() = 0;
+            virtual void onDeclareResources() = 0;
 
             // Execute pass code in this method
-            virtual void recordCommands(VkCommandBuffer) = 0;
+            virtual void onRecordCommands(VkCommandBuffer) = 0;
 
             // Retrieves command queue family for this pass
             [[nodiscard]] CommandQueueFamily getCommandQueueFamily() const { return commandQueueFamily; }
 
-        protected:
+           protected:
             // Declare read access
-            void read(const LogicalResourceAccess &resourceAccess) {
+            void read(const LogicalResourceAccess& resourceAccess) {
                 resourceReads.push_back(resourceAccess);
             }
 
             // Declare write access
-            void write(const LogicalResourceAccess &resourceAccess) {
+            void write(const LogicalResourceAccess& resourceAccess) {
                 resourceWrites.push_back(resourceAccess);
             }
 
             CommandQueueFamily commandQueueFamily;
-            ResourceManager &resourceManager;
+            ResourceManager& resourceManager;
             std::vector<LogicalResourceAccess> resourceReads;
             std::vector<LogicalResourceAccess> resourceWrites;
         };
-    }
-};
+
+        class LogicalPassBuilder {
+           public:
+            using CreateCallback = std::function<void()>;
+            using ReleaseCallback = std::function<void()>;
+            using DeclareResourcesCallback = std::function<void(ILogicalRenderPass&)>;
+            using RecordCommandsCallback = std::function<void(VkCommandBuffer)>;
+
+            static LogicalPassBuilder create(const CommandQueueFamily& queueFamily, ResourceManager& rm) {
+                return LogicalPassBuilder(queueFamily, rm);
+            }
+
+            LogicalPassBuilder& onCreate(CreateCallback cb) {
+                onCreateCb = std::move(cb);
+                return *this;
+            }
+            LogicalPassBuilder& onRelease(ReleaseCallback cb) {
+                onReleaseCb = std::move(cb);
+                return *this;
+            }
+            LogicalPassBuilder& onDeclareResources(DeclareResourcesCallback cb) {
+                onDeclareResourcesCb = std::move(cb);
+                return *this;
+            }
+            LogicalPassBuilder& onRecordCommands(RecordCommandsCallback cb) {
+                onRecordCommandsCb = std::move(cb);
+                return *this;
+            }
+
+            std::unique_ptr<ILogicalRenderPass> build(){
+                struct LambdaPass : ILogicalRenderPass {
+                    LambdaPass(const CreateInfo& info,
+                               CreateCallback onCreate,
+                               ReleaseCallback onRelease,
+                               DeclareResourcesCallback onDeclareResources,
+                               RecordCommandsCallback onRecordCommands)
+                        : ILogicalRenderPass(info),
+                          onCreateCb(std::move(onCreate)),
+                          onReleaseCb(std::move(onRelease)),
+                          onDeclareResourcesCb(std::move(onDeclareResources)),
+                          onRecordCommandsCb(std::move(onRecordCommands)) {}
+
+                    void onCreate() override {
+                        if (onCreateCb) onCreateCb();
+                    }
+                    void onRelease() override {
+                        if (onReleaseCb) onReleaseCb();
+                    }
+                    void onDeclareResources() override {
+                        if (onDeclareResourcesCb) onDeclareResourcesCb(*this);
+                    }
+                    void onRecordCommands(VkCommandBuffer cmd) override {
+                        if (onRecordCommandsCb) onRecordCommandsCb(cmd);
+                    }
+
+                    CreateCallback onCreateCb;
+                    ReleaseCallback onReleaseCb;
+                    DeclareResourcesCallback onDeclareResourcesCb;
+                    RecordCommandsCallback onRecordCommandsCb;
+                };
+
+                ILogicalRenderPass::CreateInfo info{
+                    .commandQueueFamily = queueFamily,
+                    .resourceManager = resourceManager};
+
+                return std::make_unique<LambdaPass>(
+                    info,
+                    std::move(onCreateCb),
+                    std::move(onReleaseCb),
+                    std::move(onDeclareResourcesCb),
+                    std::move(onRecordCommandsCb));
+            }
+
+           private:
+            LogicalPassBuilder(const CommandQueueFamily& qf, ResourceManager& rm)
+                : queueFamily(qf), resourceManager(rm) {}
+
+            CommandQueueFamily queueFamily;
+            ResourceManager& resourceManager;
+            CreateCallback onCreateCb;
+            ReleaseCallback onReleaseCb;
+            DeclareResourcesCallback onDeclareResourcesCb;
+            RecordCommandsCallback onRecordCommandsCb;
+        };
+    }  // namespace Rendergraph
+};  // namespace Hammock
